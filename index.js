ee2a434355ee6569b2bbf0b0b1a6ece6b3cf6b5e
Arkm20 <101435917+Arkm20@users.noreply.github.com>
2024-08-07 22:15:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import path from 'node:path';
 import cors from 'cors';
 
 const __dirname = process.cwd();
-const server = http.createServer();
-const app = express(server);
+const app = express();
+const server = http.createServer(app);
 const bareServer = createBareServer('/outerspace/');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
